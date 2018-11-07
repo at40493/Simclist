60707227ed34ce74ecec6c5a799e90a37098ce99
at40493 <at9247@gmail.com>
2018-11-07 18:18:04 +0800

    Modify ret value and check the value of str.

--- main.c
+++ main.c
@@ -8,56 +8,49 @@
 int main()
 {
 	list_t list;
-	int num, ret = 0, *result;
+	int num, ret = -1, *result;
 	char *str = NULL, *endptr;
 	size_t len = 0;
 
 	if(list_init(&list) != 0) { // initial
 		fprintf(stderr, "Init failed\n");
-		ret = -1;
-		goto exit;
+		return ret;
 	}
 	fprintf(stdout, "Insert your number: ");
 	if(getline(&str, &len, stdin) <= 0) {
 		fprintf(stderr, "The input isn't a number.\n");
-		ret = -1;
 		goto exit;
 	}
 	errno = 0 ; /* To distinguish success/failure after call */
 	num = strtol(str, &endptr, 10); //String to int
 
-
 	/* Check for various possible errors */
 
-	if((errno == ERANGE && (num == LONG_MAX || num == LONG_MIN))
-	   || (errno != 0 && num == 0)) {
+	if(errno == ERANGE) {
 		perror("strtol");
-		ret = -1;
 		goto exit;
 	}
 
 	if(endptr == str) {
 		fprintf(stderr, "No digits were found\n");
-		ret = -1;
 		goto exit;
 	}
 
 	if(list_append(&list, &num) < 0) { // Add an element to the list
 		fprintf(stderr, "Add data failed.\n");
-		ret = -1;
 		goto exit;
 	}
 	result = (int*)list_get_at(&list, 0); // Get the first element.
 	if(result == NULL) {
 		fprintf(stderr, "Print the result failed.\n");
-		ret = -1;
 		goto exit;
 	}
 
 	fprintf(stdout, "Your number was: %d\n", *result);
-
+	ret = 0;
 exit:
 	list_destroy(&list);
-	free(str);
+	if(str != NULL) // str isn't NULL
+		free(str);
 	return ret;
 }
